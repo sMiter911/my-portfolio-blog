4e99b2b84cc442904afece8040469c94078bbf5f
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-02-05 12:53:36 +0200

    WIP:404-styling

--- pages/404.js
+++ pages/404.js
@@ -1,35 +1,46 @@
-import Link from 'next/link'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     setTimeout(() => {
-      router.push('/')
-    }, 4000)
-  }, [])
+      router.push("/");
+    }, 4000);
+  }, []);
 
   return (
-    <div className="not-found">
-      <h1>404</h1>
-      <h2>Oops! That page cannot be found 😭</h2>
-      <p>Redirecting to <Link href="/"><a>Homepage</a></Link></p>
+    <section className="home section">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <div className="not-found">
+            <h1>404</h1>
+            <h2>Oops! That page cannot be found 😭</h2>
+            <p>
+              Redirecting to{" "}
+              <Link href="/">
+                <a>Homepage</a>
+              </Link>
+            </p>
 
-      <style jsx>{`
-        .not-found {
-          background: #fff;
-          padding: 30px;
-          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-          transform: rotateZ(-1deg);
-        }
-        h1 {
-          font-size: 3em;
-        }
-      `}</style>
-    </div>
+            <style jsx>{`
+              .not-found {
+                background: #fff;
+                padding: 30px;
+                box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+                transform: rotateZ(-1deg);
+              }
+              h1 {
+                font-size: 3em;
+              }
+            `}</style>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
- 
+};
+
 export default NotFound;
