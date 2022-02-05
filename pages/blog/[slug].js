9ffacee3b54d18e7fcae4b0d5d4d0e47abd1ecd2
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-02-05 12:33:24 +0200

    WIP:Fallback-Fix

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -37,6 +38,8 @@ export async function getStaticProps({ params }) {
 }
 
 export default function BlogDetails({ blogPost }) {
+  if(!blogPost) return <Skeleton/>
+
   const { title, featuredImage, post } = blogPost.fields;
 
   return (
