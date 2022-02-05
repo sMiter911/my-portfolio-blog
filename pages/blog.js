7a230dc520066424e1d6e8cb0a3ed70087d31be4
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-02-05 09:52:35 +0200

    Blog Added

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,51 @@
 import Head from "next/head";
+import { createClient } from "contentful";
+import BlogPostCard from "../components/BlogPostCard";
 
-const Blog = () => {
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({
+    content_type: "blogPost",
+  });
+
+  return {
+    props: {
+      blogPosts: res.items,
+    },
+  };
+}
+
+export default function Blog({ blogPosts }) {
+  console.log(blogPosts);
   return (
     <>
       <Head>
         <title>Wandu | Blog</title>
         <meta name="keywords" content="wandu" />
       </Head>
+      <section className="home section">
+        <div className="home__container container grid">
+          <div className="home__content grid">
+            <div className="recipe-list">
+              {blogPosts.map((post) => (
+                <BlogPostCard key={post.sys.id} post={post} />
+              ))}
+
+              <style jsx>{`
+                .recipe-list {
+                  display: grid;
+                  grid-template-columns: 1fr 1fr;
+                  grid-gap: 20px 60px;
+                }
+              `}</style>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
-};
-export default Blog;
+}
---
+++ pages/blog/[slug].js
@@ -0,0 +1,91 @@
+import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "blogPost",
+  });
+
+  const paths = res.items.map((post) => {
+    return {
+      params: { slug: post.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "blogPost",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      blogPost: items[0],
+    },
+  };
+}
+
+export default function BlogDetails({ blogPost }) {
+  const { title, featuredImage, post } = blogPost.fields;
+
+  return (
+    <section className="home section">
+      <div className="home__container container grid">
+        <div>
+          <div className="banner">
+            <Image
+              src={"https:" + featuredImage.fields.file.url}
+              height={featuredImage.fields.file.details.image.height}
+              width={featuredImage.fields.file.details.image.width}
+              alt={featuredImage.fields.title}
+            />
+            <h2>{title}</h2>
+          </div>
+          <div className="blog-post">
+            <br />
+            <div>{documentToReactComponents(post)}</div>
+          </div>
+
+          <style jsx>{`
+            h2,
+            h3 {
+              text-transform: uppercase;
+            }
+            .banner h2 {
+              margin: 0;
+              background: #fff;
+              display: inline-block;
+              padding: 20px;
+              position: relative;
+              top: -60px;
+              left: -10px;
+              transform: rotateZ(-1deg);
+              box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+            }
+            .info p {
+              margin: 0;
+            }
+            .info span::after {
+              content: ", ";
+            }
+            .info span:last-child::after {
+              content: ".";
+            }
+          `}</style>
+        </div>
+      </div>
+    </section>
+  );
+}
