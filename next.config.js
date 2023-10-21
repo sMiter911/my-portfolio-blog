2c2d8430edb6bcf9a16103a1f1d0a2f1a3315691
Wandu <31390092+sMiter911@users.noreply.github.com>
2023-10-21 15:00:13 +0200

    next config fs fallback

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.ctfassets.net'],
-  }
-}
+    domains: ["images.ctfassets.net"],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+};
