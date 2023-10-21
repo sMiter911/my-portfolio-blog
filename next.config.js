67ce8a9f02d343a2878cbd5c534fba9e39062f5e
Wandu <31390092+sMiter911@users.noreply.github.com>
2023-10-21 15:03:48 +0200

    next config update

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ module.exports = {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
       config.resolve.fallback = {
         fs: false,
+        net: false,
+        dns: false,
+        child_process: false,
+        tls: false,
       };
     }
 
