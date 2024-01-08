1879a6c39ec64dbf815ec44c1f487aab9871f92e
Wandu <31390092+sMiter911@users.noreply.github.com>
2024-01-09 00:06:45 +0200

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
-
-
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
